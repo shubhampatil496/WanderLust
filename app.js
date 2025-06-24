a5b98c2c3ed31566465fcaef8694f8c17f43bda9
shubham Pati <patilshubham1261@gmail.com>
2025-06-24 14:08:17 +0530

    add routes

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
@@ -38,6 +39,40 @@ app.get("/listings", async (req,res) => {
 //New Route
 app.get("/listings/new", (req,res) => {
     res.render("./listings/new.ejs");
+});
+
+app.post("/listings", async(req,res) => {
+    const {title,description,image,price,location,country} = req.body;
+    const listing1 = new listing({
+        title:title,
+        description:description,
+        image:image,
+        price:price,
+        location:location,
+        country:country
+    });
+    await listing1.save();
+    res.redirect("/listings");
+});
+
+//Edit Route : Edit listing
+app.get("/listings/:id/edit", async (req,res) => {
+    let {id} = req.params;
+    const listingData = await listing.findById(id);
+    res.render("./listings/edit.ejs", {listingData});
+});
+
+app.put("/listings/:id", async (req,res) => {
+    let {id} = req.params;
+    await listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//Delete Route : Delete Listing
+app.delete("/listing/:id", async (req,res) => {
+    let {id} = req.params;
+    await listing.findByIdAndDelete(id);
+    res.redirect("/listings");
 });
 
 //Show Route : Detailed Information About List
