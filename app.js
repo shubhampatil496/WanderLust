50c69fd830ccfc1740b17900d8c2891f69decf24
shubham Pati <patilshubham1261@gmail.com>
2025-06-25 12:42:58 +0530

    add

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const listing = require("./models/listing.js");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 
 main()
@@ -20,7 +21,8 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
