bb549acc77e2ee19289ba838dd217608acbe6881
shubham Pati <patilshubham1261@gmail.com>
2025-07-02 09:45:38 +0530

    .

--- models/listing.js
+++ models/listing.js
@@ -2,39 +2,44 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title:{
-        type: String,
-        required:true
-    },
-    description:{
-        type: String
-    },
-    image: {
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
     filename: {
-        type: String,
-        default: "listingimage"
+      type: String,
+      default: "listingimage",
     },
     url: {
-        type: String,
-        default: "https://images.unsplash.com/photo-1749460396814-1b735ac8c7e3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0M3x8fGVufDB8fHx8fA%3D%3D",
-        set: (v) =>
-            v === " " ? "https://images.unsplash.com/photo-1749460396814-1b735ac8c7e3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0M3x8fGVufDB8fHx8fA%3D%3D" : v
-    }
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1749460396814-1b735ac8c7e3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0M3x8fGVufDB8fHx8fA%3D%3D",
+      set: (v) =>
+        v === " "
+          ? "https://images.unsplash.com/photo-1749460396814-1b735ac8c7e3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0M3x8fGVufDB8fHx8fA%3D%3D"
+          : v,
+    },
   },
 
-    price:{
-        type: Number
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
     },
-    location:{
-        type: String
-    },
-    country:{
-        type: String
-    },
-    reviews: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Review"
-    }
+  ],
 });
 
 const listing = mongoose.model("listing", listingSchema);
