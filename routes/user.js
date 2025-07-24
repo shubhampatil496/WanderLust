5268b6e0cb138afaffb699b82c4a7b57753eb092
shubham Pati <patilshubham1261@gmail.com>
2025-07-24 14:08:49 +0530

    ADD : Signup Functionality

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 
 router.get("/signup", (req,res) => {
     res.render("users/signup.ejs");
 });
 
-router.post("/signup", async (req,res) => {
-    let {username, email, password} = req.body;
+router.post("/signup", wrapAsync (async (req,res) => {
+    try{
+        let {username, email, password} = req.body;
     const newUser = new User({email, username});
-    User.register(newUser, password);
-})
+    const registerdUser = await User.register(newUser, password);
+    console.log(registerdUser);
+    req.flash("success", "WelCome to Wanderlust");
+    res.redirect("/listings");
+    } catch(err){
+       req.flash("error", err.message); 
+       res.redirect("/signup");
+    }
+}));
 
 module.exports = router;
