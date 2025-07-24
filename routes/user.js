6ab3389001e0206be57e4c321c5c66a1687813b6
shubham Pati <patilshubham1261@gmail.com>
2025-07-24 15:29:12 +0530

    Add: Login Fuctionality

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 router.get("/signup", (req,res) => {
     res.render("users/signup.ejs");
@@ -20,5 +21,20 @@ router.post("/signup", wrapAsync (async (req,res) => {
        res.redirect("/signup");
     }
 }));
+
+//Login Routes
+router.get("/login", (req,res) => {
+    res.render("users/login.ejs");
+});
+
+router.post("/login",passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash : true,
+    }), 
+    async(req,res) => {
+    req.flash("success", "Login Successfully");
+    res.redirect("/listings");
+
+});
 
 module.exports = router;
